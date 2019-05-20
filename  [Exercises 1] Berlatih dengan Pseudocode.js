75b0b579b66e276d901308dad6f20ae90a111e9f
dwitama alfred <dwitamaalfred@dwitamas-MacBook-Pro.local>
2019-05-20 19:49:46 +0700

    update tugas

---
+++  [Exercises 1] Berlatih dengan Pseudocode.js
@@ -0,0 +1,46 @@
+
+// Newton Second Law
+
+//store "m" with 600 kg
+//store "a" with 2 m/s2
+//store "ΣF" without any value in NEWTON
+//CALCULATE "m" times "a"
+//SET "ΣF" value with calculation result
+//DISPLAY "1200 NEWTON"
+
+// Tahun Kabisat
+
+//store “year” with number value
+//calculate “year” mod 4 mod100 mod400
+//if "count" equals to 0
+    //display "kabisat"
+//else
+    //display "non-kabisat"
+//calculate “year” mod 4 and mod 100
+//if "count" equals to > 0
+    //display "kabisat"
+//else
+    //display "non-kabisat"
+
+// Laundry Day
+
+//store “dirty clothes” with 20
+//while “dirty clothes” < 0
+//subtract “dirty clothes” by 1
+//if “dirty clothes” < 0
+	//display “not all yet”
+//if “dirty clothes” equals to 0
+	//do “start”
+
+// Periksa Kuku
+
+//store “student” with 40
+//store “nails” with number value in mm
+//while “student” > 0
+	//do “periksa kuku anak-anak”
+//if “nails” equals to 0 mm
+	//do “say : anak pinter”
+//else
+	//do “hukum”
+//if “student” equals to 0
+	//do “do next task”
---
+++ [Exercises 3] Mengenal penggunaan Conditional if-else dalam JavaScript.js
@@ -0,0 +1,22 @@
+var nama = 'Zero';
+var peran = 'Penyihir';
+
+if (nama === '' && peran === ''){
+    console.log('Nama harus diisi');
+}
+
+else if (nama !== '' && peran === ''){
+    console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
+}
+
+else if (nama !== '' && peran === 'Ksatria'){
+    console.log('Selamat datang di Dunia Proxytia, ' + nama +'.' + ' Halo ' + peran + ' ' + nama + ' kamu dapat menyerang dengan senjatamu!');
+}
+
+else if (nama !== '' && peran === 'Tabib'){
+    console.log('Selamat datang di Dunia Proxytia, ' + nama +'.' + ' Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka.');
+}
+
+else if (nama !== '' && peran === 'Penyihir'){
+    console.log('Selamat datang di Dunia Proxytia, ' + nama +'.' + '  Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+}

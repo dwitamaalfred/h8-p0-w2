dcbaa89f31b57b9d85872c0f59d9397cece9d0be
Dwitama Alfred <50652650+dwitamaalfred@users.noreply.github.com>
2019-05-24 20:48:45 +0700

    Update and rename  [Exercises 12] Konversi Menit.js to [Exercises 12] Konversi Menit.js

--- [Exercises 12] Konversi Menit.js
+++ [Exercises 12] Konversi Menit.js
@@ -1,4 +1,4 @@
-/*function konversiMenit(menit) { 
+function konversiMenit(menit) { 
     var jam = Math.floor(menit / 60);
     var sisaMenit = menit % 60;
     if(sisaMenit <= 9) {
@@ -10,14 +10,4 @@
   console.log(konversiMenit(124));
   console.log(konversiMenit(53));
   console.log(konversiMenit(88));
-  console.log(konversiMenit(120));*/
-
-  function konversiMenit(menit) {
-    var menit1 = 0;
-    var jam = 0;
-    
-      menit1 += menit%60;
-      jam = (menit - menit1) / 60 ;
-
-      return jam + ':' + menit1;
-  }
+  console.log(konversiMenit(120));
